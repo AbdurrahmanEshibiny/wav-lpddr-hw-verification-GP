3c2e3ca9b321da8195bc61e2f9db412318e5c2a9
SamuelShokry <samuel.shokry@gmail.com>
2022-07-04 00:57:50 +0200

    Completing power_down test

--- verif/sv/sequences/dfi/wddr_DFI_power_down_seq.sv
+++ verif/sv/sequences/dfi/wddr_DFI_power_down_seq.sv
@@ -1,3 +1,9 @@
+class my_rand;
+    randc bit [3:0] ba;
+
+    constraint c1{ba inside {[0:15]};};
+endclass
+
 class wddr_DFI_power_down_seq extends wddr_base_seq;
 
     `uvm_object_utils(wddr_DFI_power_down_seq)
@@ -7,70 +13,119 @@ class wddr_DFI_power_down_seq extends wddr_base_seq;
     endfunction
 
     virtual task body();
-        // wav_DFI_lp_transfer trans = new();
+        // wav_DFI_lp_transfer vif <= new();
         virtual wav_DFI_if vif;
-        wav_DFI_write_transfer trans = new();
+        // wav_DFI_write_transfer vif <= new();
+        my_rand random;
         int err;
         super.body();
         ddr_boot(err);
         if (err != 0) begin
-            `uvm_error(get_name(), $sformatf("sequence err_cnt = %d ", err_cnt));
+            `uvm_error(get_name(), $sformatf("sequence err_cnt <= %d ", err_cnt));
         end
         config_vips(200,1);
-
+        
+        random = new();
         if (!uvm_config_db#(virtual wav_DFI_if)::get(uvm_root::get(), "*", "DFI_vif", vif)) begin
             `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".DFI_vif"});
         end
 
     ///////////////////////////Power Down Entry /////////////////////////////////
-    `uvm_info(get_name(), "Starting power down entry", UVM_MEDIUM);
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "power down entry", UVM_MEDIUM);
+        vif.cke[0]  <= 2'b01;
+        vif.cs[0]	<= 2'b10;
+        vif.cs[2]	<= 2'b00;
+        vif.dram_clk_disable[0] <= 0;
+        vif.dram_clk_disable[1] <= 1;
+        vif.dram_clk_disable[2] <= 1;
+        vif.dram_clk_disable[3] <= 1;
 
-    trans.cs[0]	= 2'b11;
-    trans.dram_clk_disable[0] = 0;
-    trans.dram_clk_disable[1] = 0;
-    trans.dram_clk_disable[2] = 0;
-    trans.dram_clk_disable[3] = 0;
-    trans.address[0] = 14'h04;
-    `uvm_send(trans);
+        vif.address[0] <= 14'h04;
+        vif.address[2] <= 14'h00;
+        // `uvm_send(vif);
 
-    // //posedge
-    // @(posedge vif.mp_drv.cb_drv);
-    // trans.cs[0]	= 2'b01;
-    // trans.address[0] = 14'b0000001_0000001;
-    // `uvm_send(trans);
- 	// // {ch0_vif.ca0,ch0_vif.ca1,ch0_vif.ca2,ch0_vif.ca3,ch0_vif.ca4,ch0_vif.ca5,ch0_vif.ca6}	=	7'b0000001;
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "power down exit", UVM_MEDIUM);
+        vif.address[0] <= 14'h00;
+        vif.cs[0]	<= 2'b10;
+        vif.cs[2]	<= 2'b10;
+        // `uvm_send(vif);
+        
+    //////////////////////////Per Bank refresh /////////////////////////////////
+        /*
+            enter a for loop that does 2 things: 
+            1) at posedge send all bank refresh command
+            2) at nededge randomize the bank address and send it on the CA Bus
+        */
+        repeat(4) @(posedge vif.clock);
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "Per Bank refresh ", UVM_MEDIUM);
+        vif.address[0] <= 0;
+        vif.address[2] <= 7'b0111000;
+        for(int i = 0; i < 16; i++) begin
+            assert(random.randomize());
+            @(posedge vif.clock);
+            `uvm_info(get_name(), $psprintf("Per Bank refresh, bank=%0h", random.ba), UVM_MEDIUM);
+            vif.address[0] <= {7'b000, random.ba};
+            vif.address[2] <= 7'b0111000;
+        end     
+        
+        //////////////////////////All Bank Refresh /////////////////////////////////
+        repeat(4) @(posedge vif.clock);
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "All Bank refresh ", UVM_MEDIUM);
+        vif.address[0] <= 7'b0111000;
+        vif.address[2] <= 7'b1111000;
 
-    // //negedge -> CS must be 0 while in power down
-    // @(posedge vif.mp_drv.cb_drv);
-    // `uvm_info(get_name(), "toggling the chip select", UVM_MEDIUM);    
-    // trans.cs[0]	= 2'b00;
-    // `uvm_send(trans);
+
+        //////////////////////////Deep Sleep Entry with power down/////////////////////////////////
+        repeat(4) @(posedge vif.clock);
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "Deep Sleep Entry with power down", UVM_MEDIUM);
+        vif.address[0] <= 7'b1101000;
+        vif.address[2] <= 7'b1000000;
+        
+        @(posedge vif.clock);
+        vif.cs[0] <= 2'b00;
+        vif.cs[2] <= 2'b00;
+
+        //////////////////////////Deep Sleep Exit  with power down/////////////////////////////////
+        repeat(4) @(posedge vif.clock);
+        `uvm_info(get_name(), "Deep Sleep Exit with power down", UVM_MEDIUM);
+        vif.cs[0] <= 2'b10;
+        vif.cs[2] <= 2'b10;
+        
+        //////////////////////////Deep Sleep Entry with self refresh /////////////////////////////////
+        repeat(4) @(posedge vif.clock);
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "Deep Sleep Entry with self refresh", UVM_MEDIUM);
+        vif.address[0] <= 7'b1101000;
+        vif.address[2] <= 7'b1000000;
+        @(posedge vif.clock);
+        vif.cs[0] <= 2'b00;
+        vif.cs[2] <= 2'b00;
+
+        //////////////////////////Deep Sleep Exit with self refresh /////////////////////////////////
+        repeat(4) @(posedge vif.clock);
+        `uvm_info(get_name(), "Deep Sleep Exit with self refresh", UVM_MEDIUM);
+        vif.cs[0] <= 2'b10;
+        vif.cs[2] <= 2'b10;
 
 
-	// //////////////////////////Power Down Exit /////////////////////////////////
-    // `uvm_info(get_name(), "Starting power down exit", UVM_MEDIUM);
-    // // #3	// if CS toggles then we exit power down
-    // @(posedge vif.mp_drv.cb_drv);
-    // `uvm_info(get_name(), "toggling the chip select", UVM_MEDIUM);  
-    // trans.cs[0] =	2'b01;
-    // `uvm_send(trans);
-    #100us;
+        //////////////////////////Deep Sleep Entry  /////////////////////////////////
+        repeat(4) @(posedge vif.clock);        
+        @(posedge vif.clock);
+        `uvm_info(get_name(), "Deep Sleep Entry", UVM_MEDIUM);
+        vif.address[0] <= 7'b1101000;
+        vif.address[2] <= 7'b0000000;
+        
+        //////////////////////////Deep Sleep Exit /////////////////////////////////
+        repeat(4) @(posedge vif.clock);  
+        `uvm_info(get_name(), "Deep Sleep Exit", UVM_MEDIUM);
+        vif.address[0] <= 7'b0101000;
 
-        // `uvm_info(get_type_name(), $psprintf("1.PRE-CREATE OF TRANSACTION"), UVM_LOW);
-        // `uvm_create(trans);
-        // `uvm_info(get_type_name(), $psprintf("2.POST-CREATE, PRE-RUN OF TRANSACTION"), UVM_LOW);
-
-        // trans.req = 1'b1;
-        // trans.is_ctrl = 1'b1;
-
-        // `uvm_rand_send(trans);
-
-        // `uvm_info(get_type_name(), $psprintf("3.POST-CREATE, PPOST-RUN, PRE-RSP OF TRANSACTION"), UVM_LOW);
-
-        // `uvm_info(get_type_name(), "--------PRINTING THE REQ ITEM--------", UVM_DEBUG); 
-        // trans.print();
-        // get_response(rsp);  
-        `uvm_info(get_type_name(), "done sequence", UVM_LOW);
+        repeat(4) @(posedge vif.clock); 
     endtask
 
 endclass
