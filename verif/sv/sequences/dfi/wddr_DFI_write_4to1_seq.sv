f40e54ab1504c1c6b6d97c01edaedb0d6db017f3
SamuelShokry <samuel.shokry@gmail.com>
2022-07-07 23:36:15 +0200

    modifying write 4to1 seq

--- verif/sv/sequences/dfi/wddr_DFI_write_4to1_seq.sv
+++ verif/sv/sequences/dfi/wddr_DFI_write_4to1_seq.sv
@@ -3,29 +3,24 @@ class wddr_DFI_write_4to1_seq extends wddr_base_seq;
 
     function new(string name="wddr_DFI_write_4to1_seq");
         super.new(name);
+        freqRatio = 2;  // to set the frequency to 4:1
     endfunction
 
     virtual task body();
         wav_DFI_write_transfer trans = new();
         int err;
 
-        freqRatio = 2;  // to set the frequency to 4:1
-
         super.body();
-        // ddr_boot(err);
+        //ddr_boot(err);
         phy_bringup(err);
         set_dfi_wck_mode(1);
+
         if (err != 0) begin
             `uvm_error(get_name(), $sformatf("sequence err_cnt = %d ", err_cnt));
         end
-        `uvm_info(get_type_name(), $psprintf("1.PRE-CREATE OF TRANSACTION"), UVM_LOW);
-        `uvm_create(trans);
-        `uvm_info(get_type_name(), $psprintf("2.POST-CREATE, PRE-RUN OF TRANSACTION"), UVM_LOW);
-        
-        
 
-        // you can determine the transaction values here
 
+        // main test code
 
         // sendin the transaction
         `uvm_send(trans);
