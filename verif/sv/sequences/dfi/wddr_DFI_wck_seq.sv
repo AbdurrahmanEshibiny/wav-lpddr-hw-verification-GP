bf0d95e013ad18a5c5c9ba936a45ba6e168e4b79
SamuelShokry <samuel.shokry@gmail.com>
2022-07-07 02:27:20 +0200

    wck test for 4_1 ratio

--- verif/sv/sequences/dfi/wddr_DFI_wck_seq.sv
+++ verif/sv/sequences/dfi/wddr_DFI_wck_seq.sv
@@ -7,10 +7,10 @@ typedef struct packed {
     // bit                  i_dfi_cs_p1;
     // bit                  i_dfi_cs_p2;
     // bit                  i_dfi_cs_p3;
-    // bit                  i_dfi_dram_clk_disable_p0;
-    // bit                  i_dfi_dram_clk_disable_p1;
-    // bit                  i_dfi_dram_clk_disable_p2;
-    // bit                  i_dfi_dram_clk_disable_p3;
+    bit                  i_dfi_dram_clk_disable_p0;
+    bit                  i_dfi_dram_clk_disable_p1;
+    bit                  i_dfi_dram_clk_disable_p2;
+    bit                  i_dfi_dram_clk_disable_p3;
  
     // // bit                      i_dfi_parity_in_p0,
     // // bit                      i_dfi_parity_in_p1,
@@ -32,14 +32,14 @@ typedef struct packed {
     // bit [1:0]                i_dfi_wck_cs_p1;
     // bit [1:0]                i_dfi_wck_cs_p2;
     // bit [1:0]                i_dfi_wck_cs_p3;
-    bit                      i_dfi_wck_en_p0;
-    bit                      i_dfi_wck_en_p1;
-    bit                      i_dfi_wck_en_p2;
-    bit                      i_dfi_wck_en_p3;
-    bit [1:0]                i_dfi_wck_toggle_p0;
-    bit [1:0]                i_dfi_wck_toggle_p1;
-    bit [1:0]                i_dfi_wck_toggle_p2;
-    bit [1:0]                i_dfi_wck_toggle_p3;
+    // bit                      i_dfi_wck_en_p0;
+    // bit                      i_dfi_wck_en_p1;
+    // bit                      i_dfi_wck_en_p2;
+    // bit                      i_dfi_wck_en_p3;
+    // bit [1:0]                i_dfi_wck_toggle_p0;
+    // bit [1:0]                i_dfi_wck_toggle_p1;
+    // bit [1:0]                i_dfi_wck_toggle_p2;
+    // bit [1:0]                i_dfi_wck_toggle_p3;
 } wck_control;
 
 class wck_c;
@@ -63,15 +63,15 @@ class wddr_DFI_wck_seq extends wddr_base_seq;
         // vif.wck_cs[2] <= item.i_dfi_wck_cs_p2;
         // vif.wck_cs[3] <= item.i_dfi_wck_cs_p3;
 
-        vif.wck_en[0] <= item.i_dfi_wck_en_p0;
-        vif.wck_en[1] <= item.i_dfi_wck_en_p1;
-        vif.wck_en[2] <= item.i_dfi_wck_en_p2;
-        vif.wck_en[3] <= item.i_dfi_wck_en_p3;
+        // vif.wck_en[0] <= item.i_dfi_wck_en_p0;
+        // vif.wck_en[1] <= item.i_dfi_wck_en_p1;
+        // vif.wck_en[2] <= item.i_dfi_wck_en_p2;
+        // vif.wck_en[3] <= item.i_dfi_wck_en_p3;
 
-        vif.wck_toggle[0] <= item.i_dfi_wck_toggle_p0;
-        vif.wck_toggle[1] <= item.i_dfi_wck_toggle_p1;
-        vif.wck_toggle[2] <= item.i_dfi_wck_toggle_p2;
-        vif.wck_toggle[3] <= item.i_dfi_wck_toggle_p3;
+        // vif.wck_toggle[0] <= item.i_dfi_wck_toggle_p0;
+        // vif.wck_toggle[1] <= item.i_dfi_wck_toggle_p1;
+        // vif.wck_toggle[2] <= item.i_dfi_wck_toggle_p2;
+        // vif.wck_toggle[3] <= item.i_dfi_wck_toggle_p3;
 
         // vif.cke[0] <= item.i_dfi_cke_p0;
         // vif.cke[1] <= item.i_dfi_cke_p1;
@@ -83,10 +83,10 @@ class wddr_DFI_wck_seq extends wddr_base_seq;
         // vif.cs[2] <= item.i_dfi_cs_p2;
         // vif.cs[3] <= item.i_dfi_cs_p3;
     
-        // vif.dram_clk_disable[0] <= item.i_dfi_dram_clk_disable_p0;
-        // vif.dram_clk_disable[1] <= item.i_dfi_dram_clk_disable_p1;
-        // vif.dram_clk_disable[2] <= item.i_dfi_dram_clk_disable_p2;
-        // vif.dram_clk_disable[3] <= item.i_dfi_dram_clk_disable_p3;
+        vif.dram_clk_disable[0] <= item.i_dfi_dram_clk_disable_p0;
+        vif.dram_clk_disable[1] <= item.i_dfi_dram_clk_disable_p1;
+        vif.dram_clk_disable[2] <= item.i_dfi_dram_clk_disable_p2;
+        vif.dram_clk_disable[3] <= item.i_dfi_dram_clk_disable_p3;
 
         // vif.wrdata_cs[0] <= item.i_dfi_wrdata_cs_p0;
         // vif.wrdata_cs[1] <= item.i_dfi_wrdata_cs_p1;
@@ -118,13 +118,15 @@ class wddr_DFI_wck_seq extends wddr_base_seq;
 		bit [$bits(item.values)-1 : 0] end_val;
         bit err;
         wck_c item;
+        freqRatio = 2;
         super.body();
-        ddr_boot(err);
-        if (err != 0) begin
-            `uvm_error(get_name(), $sformatf("sequence err_cnt <= %d ", err_cnt));
-        end
-        config_vips(200,1);
+        // ddr_boot(err);
+        // if (err != 0) begin
+        //     `uvm_error(get_name(), $sformatf("sequence err_cnt <= %d ", err_cnt));
+        // end
+        // config_vips(200,2);
         set_dfi_wck_mode(1);
+        phy_bringup(err);
         item = new;
 		end_val = ~end_val;
         if (!uvm_config_db#(virtual wav_DFI_if)::get(uvm_root::get(), "*", "DFI_vif", vif)) begin
@@ -137,8 +139,9 @@ class wddr_DFI_wck_seq extends wddr_base_seq;
 			`uvm_fatal("gp_LPDDR5_monitor", "Failed to get virtual interface from config db")
 		end
 
-        repeat(20) @(posedge vif.clock);
+        repeat(50) @(posedge vif.clock);
 
+		/*
         @(posedge vif.clock);
         vif.cke[0]  <= 2'b01;
         vif.cs[0]	<= 2'b01;
@@ -161,27 +164,31 @@ class wddr_DFI_wck_seq extends wddr_base_seq;
         vif.wck_en[1] <= 0;
         vif.wck_en[2] <= 0;
         vif.wck_en[3] <= 1;
+        */
         
         for (item.values = 0; item.values < end_val; ++item.values)  begin
             //assert(item.randomize());
 			// item.values
             `uvm_info(get_name(), $psprintf("Trying values %0h", item.values), UVM_MEDIUM);            
             drive_wc(item.values);
-            repeat(40) @(posedge vif.clock);
+            repeat(50) @(posedge vif.clock);
             // if (any_signal()) begin
             //    `uvm_info(get_name(), "FINALLY!!!!!!!!!!!!!!!!!!!!!!!!!!", UVM_MEDIUM);                
             //     // break;
             // end
         end
-		
+        
+        
 		`uvm_info(get_name(), $psprintf("Trying values %0h", item.values), UVM_MEDIUM);            
         drive_wc(item.values);
         //repeat(20) @(posedge vif.clock);
 
+        /*
         `uvm_info(get_name(), $psprintf("wck st = %0h", WCKT_SL), UVM_MEDIUM);
         `uvm_info(get_name(), $psprintf("wck sh = %0h", WCKT_SH), UVM_MEDIUM);
         `uvm_info(get_name(), $psprintf("wck t = %0h", WCKT_T), UVM_MEDIUM);
         `uvm_info(get_name(), $psprintf("wck ft = %0h", WCKT_FT), UVM_MEDIUM);
+		*/
     endtask : body
 
 endclass
